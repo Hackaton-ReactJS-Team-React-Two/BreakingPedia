2eaa9781444e9a97150abcc18aff71e8dc7c32d5
Ferley Arias <ferley225@gmail.com>
2020-11-18 19:31:24 -0500

    add routes

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import "bootstrap/dist/css/bootstrap.css";
+
+import Build from '../pages/Build'
+import Home from '../pages/Home'
+import CharacterDetails from '../pages/CharacterDetails'
+import ContactUs from '../pages/ContactUs'
+import MySpace from '../pages/MySpace'
+
+
+function App() {
+  return(
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/BreakingPedia" component={Build}/>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/contact_us" component={ContactUs}/>
+        <Route exact path="/my_space" component={MySpace}/>
+        <Route exact path="/character/:name" component={CharacterDetails}/>
+      </Switch>
+    </BrowserRouter>
+  )
+}
+
+export default App
