9c78defb8b9dbc64099ef095d9b35affe84cb3de
Carlos Rodriguez <ucroxlive@gmail.com>
2020-11-23 15:19:32 -0500

    SearchInput is added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,51 +1,58 @@
-import React, { useState, useEffect, Fragment } from 'react'
+import React, { useState, useEffect, Fragment } from "react";
 
-import { getCharacters } from '../api'
+import { getCharacters } from "../api";
 
-import CharacterList from '../components/CharacterList'
+import CharacterList from "../components/CharacterList";
+import SearchInput from "../components/SearchInput";
 
 function Home() {
-  const [ characters, setCharacters ] = useState([])
-  const [ error, setError ] = useState(null)
-  const [ load, setLoad ] = useState(true)
-  const [ count, setCount ] = useState(1)
-  const [ visible, setVisible ] = useState(true)
+  const [characters, setCharacters] = useState([]);
+  const [error, setError] = useState(null);
+  const [load, setLoad] = useState(true);
+  const [count, setCount] = useState(1);
+  const [visible, setVisible] = useState(true);
   useEffect(() => {
     async function fetchData() {
-      setLoad(true)
-      setError(null)
+      setLoad(true);
+      setError(null);
 
       try {
-        const data = await getCharacters(count, 8)
-        setCharacters(characters.concat(data))
-        console.log(characters.length+data.length)
-        if(characters.length+data.length===63) {
-          setVisible(false)
+        const data = await getCharacters(count, 8);
+        setCharacters(characters.concat(data));
+        console.log(characters.length + data.length);
+        if (characters.length + data.length === 63) {
+          setVisible(false);
         }
-        setLoad(false)
+        setLoad(false);
       } catch (error) {
-        setLoad(false)
-        setError(error)
+        setLoad(false);
+        setError(error);
       }
     }
-    fetchData()
-  }, [ count ])
-  if(error) {
-    return(
-      <h1>{error.message}</h1>
-    )
+    fetchData();
+  }, [count]);
+  if (error) {
+    return <h1>{error.message}</h1>;
   }
-  if(load) {
-    <h1>Loading</h1>
+  if (load) {
+    <h1>Loading</h1>;
   }
   return (
     <Fragment>
-      <CharacterList characters={characters}/>
-      <button onClick={() => {
-        setCount(count +1)
-      }} className={`${visible? "d-block": "d-none"} btn btn-dark mx-auto my-5`}>Load more</button>
+      <SearchInput />
+      <CharacterList characters={characters} />
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+        className={`${
+          visible ? "d-block" : "d-none"
+        } btn btn-dark mx-auto my-5`}
+      >
+        Load more
+      </button>
     </Fragment>
-  )
+  );
 }
 
-export default Home
+export default Home;
