21162012fb2a61e6d992956a6af39413e4ef1354
Carlos Rodriguez <ucroxlive@gmail.com>
2020-11-24 15:58:00 -0500

    add component quotes

--- src/pages/CharacterDetails.js
+++ src/pages/CharacterDetails.js
@@ -1,9 +1,32 @@
 import React from "react";
+import { getQuoteByName } from "../api";
 
 import CharacterDetail from "../components/CharacterDetail";
 import Comment from "../components/Comment";
 
 function CharacterDetails() {
+  const [character, setCharacter] = useState([]);
+  const [quotes, setQuotes] = useState(null);
+  const [error, setError] = useState(null);
+  const [load, setLoad] = useState(false);
+  const [count, setCount] = useState(1);
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    setLoad(true);
+    setError(null);
+    async function fetchData() {
+      try {
+        const quotes = await getQuoteByName();
+        setQuotes(quotes);
+        setLoad(false);
+      } catch (error) {
+        setLoad(false);
+        setError(error);
+      }
+    }
+    fetchData();
+  }, []);
   return (
     <div>
       <CharacterDetail />
