150e93385021e7d46db2cb45c8cd2f209d664b95
Ferley Arias <ferley225@gmail.com>
2020-11-25 16:24:28 -0500

    fix footer

--- src/components/FooterComp.js
+++ src/components/FooterComp.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";	
 import { getRandomQuote } from "../api";
 import { Form, FormControl, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
@@ -32,33 +32,56 @@ function FooterComp() {
     }
     fetchData();
   }, []);
-
   return (
     <footer>
       <div className="footer">
-        <div className="row">
-          <div className="col-sm">
-            <img src={logo} className="img_logo" alt="Logo" />
-          </div>
-          <div className="col-sm">
-            <ul>
-              <Link className="link_name">
-                <li>Home</li>
-              </Link>
-              <Link className="link_name">
-                <li>MySpace</li>
-              </Link>
-              <Link className="link_name">
-                <li>About</li>
-              </Link>
-              <Link className="link_name">
-                <li>Contact us</li>
-              </Link>
-            </ul>
+        <div class="container">
+          <div className="row">
+            <div className="col-sm">
+              <img src={logo} className="img_logo" />
+            </div>
+            <div className="col-sm">
+              <ul className="footer_ul">
+                <Link className="link_name" to="/">
+                  <li className="footer__li">Home</li>
+                </Link>
+                <Link className="link_name">
+                  <li className="footer__li">MySpace</li>
+                </Link>
+                <Link className="link_name">
+                  <li className="footer__li">About</li>
+                </Link>
+                <Link className="link_name">
+                  <li className="footer__li">Contact us</li>
+                </Link>
+              </ul>	
+            </div>	
+            <div className="col-sm form">	
+              <h6 className="footer__h6">Get our newsletter</h6>	
+              <Form inline className="content__form">	
+                <FormControl	
+                  type="text"	
+                  placeholder="email"	
+                  className="mr-sm-2 email__box"	
+                />	
+                <Button className="button_subscribe" type="submit">	
+                  Subscribe	
+                </Button>	
+              </Form>	
+              <img className="img_social" src={facebook} alt="facebook" />	
+              <img className="img_social" src={instagram} alt="instagram" />	
+              <img className="img_social" src={snapchat} alt="snapchat" />	
+              <img className="img_social" src={twitter} alt="twitter" />	
+              <img className="img_social" src={youtube} alt="youtube" />	
+            </div>	
           </div>
           <div className="row">
-            <p className="quote">“{quote != null ? quote[0].quote : ""}”</p>
-            <p className="quote">{quote != null ? quote[0].author : ""}</p>
+            <p className="quote">
+            “{quote != null ? quote[0].quote : ""}”
+            </p>
+            <p className="quote">
+            {quote != null ? quote[0].author : ""}
+            </p>
           </div>
           <div className="card-footer copyright">Copyright 2020</div>
         </div>
