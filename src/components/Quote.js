f46c3214c3252f62251b7465c4a16e9a5e2f3055
Carlos Rodriguez <ucroxlive@gmail.com>
2020-11-25 15:30:09 -0500

    create style file, styles are added to the quote component

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 
+import "./styles/Quote.css";
+
 function Quote(props) {
   return (
     <div>
-      <h1>Quotes</h1>
+      <h1 className="head_Title">Quotes</h1>
       <ul>
         {props.quotes.map((quote) => {
-          return <li>{quote.quote}</li>;
+          return <li className="quote">{quote.quote}</li>;
         })}
       </ul>
     </div>
