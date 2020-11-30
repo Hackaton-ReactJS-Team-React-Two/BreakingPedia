8cc14e5ba77bf8c1c0ff32a3ced11931bfa344d3
Carlos Rodriguez <ucroxlive@gmail.com>
2020-11-29 23:23:20 -0500

    new routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,35 @@
-import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import Build from '../pages/Build'
-import Home from '../pages/Home'
-import CharacterDetails from '../pages/CharacterDetails'
+import Build from "../pages/Build";
+import Home from "../pages/Home";
+import CharacterDetails from "../pages/CharacterDetails";
 import AboutUs from "../pages/AboutUs";
-import MySpace from '../pages/MySpace'
+import MySpace from "../pages/MySpace";
 import Layout from "../components/Layout";
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faStar } from '@fortawesome/free-regular-svg-icons'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faStar } from "@fortawesome/free-regular-svg-icons";
 
-library.add(faStar)
+library.add(faStar);
 
 function App() {
   return (
     <BrowserRouter>
       <Layout>
         <Switch>
-          <Route exact path="/BreakingPedia" component={Build} />
-          <Route exact path="/" component={Home} />
-          <Route exact path="/about_us" component={AboutUs} />
-          <Route exact path="/my_space" component={MySpace} />
-          <Route exact path="/character/:id" component={CharacterDetails} />
+          <Route exact path="/BreakingPedia" component={Home} />
+          <Route exact path="/BreakingPedia/about_us" component={AboutUs} />
+          <Route exact path="/BreakingPedia/my_space" component={MySpace} />
+          <Route
+            exact
+            path="/BreakingPedia/character/:id"
+            component={CharacterDetails}
+          />
         </Switch>
       </Layout>
     </BrowserRouter>
   );
 }
 
-export default App
+export default App;
